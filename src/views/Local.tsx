bb7d2e2a1e0df5a3086f346474e802a6b6e23e5d
Hevitriniavo <hei.tantely@gmail.com>
2024-05-03 16:34:52 +0545

    feat: jsonString

--- src/views/Local.tsx
+++ src/views/Local.tsx
@@ -1,23 +1,22 @@
 
 const Local = () => {
-    let n: number = 0;
-    const incr = () => {
-        const storedN = localStorage.getItem("n");
-        if (storedN != null) {
-            n = parseInt(storedN) + 1;
-            localStorage.removeItem("n")
-            localStorage.setItem('n', `${n}`)
+    const json = {
+        email: "elise",
+        password: "1234",
+        role: "Admin"
+    }
 
-        } else {
-            localStorage.setItem('n', '0')
-        }
-        console.log(n);
-        
+    const jsonString = JSON.stringify(json);
+
+    const showType = () => {
+        console.log(JSON.parse(jsonString));
     }
     return (
         <div>
-            <p>counter is {n} </p>
-            <button onClick={incr}>incr</button>
+            <pre>
+                {jsonString}
+            </pre>
+            <button onClick={showType}>show</button>
         </div>
     )
 }
